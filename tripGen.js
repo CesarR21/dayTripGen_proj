c4d4bfec74d02862ceba3fc4fd18c09d7ca27320
CesarR21 <88210226+CesarR21@users.noreply.github.com>
2021-07-31 09:22:37 -0500

    Add files via upload

---
+++ tripGen.js
@@ -0,0 +1,16 @@
+ let prev = document.getElementById('previous');
+ let next = document.getElementById('next');
+
+ let arr = ["img/235.jpeg"];
+ let i=0;
+
+//next btn
+
+ next.addEventListener('click',function(){
+ }
+
+//prev btn 
+ prev.addEventListener('click',function(){
+
+
+ }
