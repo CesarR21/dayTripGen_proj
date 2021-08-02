2afc3c95a506d2f1b8a0c0e1ebb16ddd69879d0d
unknown <Bizcanna21@icloud.com>
2021-08-02 11:36:15 -0500

    adding js update

---
+++ DayTripGen.js
@@ -0,0 +1,26 @@
+function generateTrip(arrayOfPlaces){
+         arrayOfPlaces=[i]
+}
+    
+let btn = document.getElementById('press to generate trip!');
+ 
+ let container= document.getElementById('Destination');
+
+ getbtn.addEventListener('click',()=>{
+  
+   for(let i = 0; i < arrayOfPlaces.length; i++);
+    
+
+    console.log(arrayOfPlaces[i]);
+    return anArray[i]
+}
+  
+  
+  
+  
+  
+       
+      
+
+
+ 
--- DayTripGen.js.js
+++ DayTripGen.js.js
@@ -14,13 +14,87 @@ let btn = document.getElementById('press to generate trip!');
     console.log(arrayOfPlaces[i]);
     return anArray[i]
 }
+
+
+
   
-  
-  
-  
-  
-       
-      
+ function generateRestaurant(arrayOfRestaurants){
+  arrayOfRestaurants=[i]
+
+ 
+let button1 = document.getElementById('press to generate food!');
+ 
+ }
+
+getbtn.addEventListener('click',()=>{
+
+for(let i = 0; i < arrayOfRestaurants.length; i++);
+
+
+console.log(arrayOfRestaurants[i]);
+return anArray[i]
+
+}
+
+
+function generateTrip(arrayOfTransport){
+  arrayOfTransport=[i]
+}
+
+let btn2 = document.getElementById('press to generate transport!');
+
+let container= document.getElementById('transportation');
+
+getbtn.addEventListener('click',()=>{
+
+for(let i = 0; i < transportation.length; i++);
+
+
+console.log(arrayOfTransportation[i]);
+return anArray[i]
+}
+
+function generateTrip(arrayOfTransport){
+  arrayOfTransport=[i]
+}
+
+let btn3 = document.getElementById('press to generate !');
+
+let container= document.getElementById('transportation');
+
+getbtn.addEventListener('click',()=>{
+
+for(let i = 0; i < transportation.length; i++);
+
+
+console.log(arrayOfTransportation[i]);
+return anArray[i]
+}
+
+function generateEntertainment(anArray){
+  console.log(anArray[0])
+  return anArray[0]
+}
+
+entertainment(["golf","cinemax","casino"]);
+
+let userInput = prompt("Enter Entertainment");
+     
+for(let i = 0; i = userInput; i++){
+    console.log(entertainment[i]);
+}   
+if(userInput===entertainment[i]);
+   console.log("entertainment"[i]);
+   
+
+
+}
+
+
+
+
+
+
 
 
  
